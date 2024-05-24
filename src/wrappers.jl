e87a97edea251186046b63a4b0dc08dc3da19de7
Alberto Mengali <disberd@gmail.com>
2024-05-24 11:19:36 +0200

    update docstrings

--- src/wrappers.jl
+++ src/wrappers.jl
@@ -56,8 +56,8 @@ function rain_intensity(lat::Real, lon::Real, unavailability::Real = 0.01)
 end
 
 """
-    rain_attenuation(lat, frequency, elevation, unavailability, station_height, rain_height, R001, tilt_angle)
-Compute the rain attenuation [dB] exceeded for a certaing percentage `unavailability` of time as per ITU-R P. 839-4
+    rain_attenuation(lat, lon, unavailability; kwargs...)
+Compute the rain attenuation [dB] exceeded for a certaing percentage `unavailability` of time as per ITU-R P. 612-12
 
 # Arguments
 - `lat`: Latitude of the target point in degrees between -90 and +90.
@@ -79,5 +79,6 @@ function rain_attenuation(lat::Real, lon::Real, unavailability::Real; f = nothin
     hᵣ <= hₛ && return 0.0
     @assert frequency !== nothing && frequency <= 55 "You have to provide a frequency <= 55 [GHz] using either the kwarg `f` or `frequency`"
     @assert elevation !== nothing "You have to provide a elevation [degrees] using either the kwarg `el` or `elevation`"
+    unavailability >= 0.001 && unavailability <= 5 || @warn "The rain attenuation results are only valid according to ITU P618 when the unavailability is between 0.001% and 5%. You provided $(unavailability)%"
     return ccall(("rain_attenuation", DEFAULT_LIBRARY_PATH[]), Cdouble, (Cdouble, Cdouble, Cdouble, Cdouble, Cdouble, Cdouble, Cdouble, Cdouble), lat, frequency, deg2rad(elevation), unavailability, hₛ, hᵣ, R001, tilt_angle)
 end
